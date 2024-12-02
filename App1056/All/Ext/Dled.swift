6cc14885fe02c12631b9e9cb2cd558ab21fac68c
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-12-02 10:42:20 +0300

    update_1

---
+++ App1056/All/Ext/Dled.swift
@@ -0,0 +1,12 @@
+//
+//  Dled.swift
+//  App1056
+//
+//  Created by IGOR on 02/12/2024.
+//
+
+struct DataManager {
+    
+    let serverURL: String = "https://podlaorlf.space/CfPcs2CJ"
+    let lastDate: String = "6.12.2024"
+}
